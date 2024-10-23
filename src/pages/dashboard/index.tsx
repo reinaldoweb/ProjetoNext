805af08c034e3d1dc0ac027bbe0d952b12ac375a
reinaldoweb <reinaldoweb.com>
2024-10-23 05:33:21 -0300

    criando rota de admin

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
   return (
@@ -11,4 +13,25 @@ export default function Dashboard() {
       <h1>Pagina Painel</h1>
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user) {
+
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+
+    props: {
+
+    },
+  };
 }
