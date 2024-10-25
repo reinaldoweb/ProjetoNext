3f9529dc33046fd4bd2fa20b69364c8246c119c4
reinaldoweb <reinaldoweb.com>
2024-10-24 22:03:46 -0300

    Criando dashboard2

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,8 @@ import styles from "./styles.module.css";
 import Head from "next/head";
 import { getSession } from "next-auth/react";
 import { Textarea } from "../../components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 export default function Dashboard() {
   return (
@@ -29,6 +31,24 @@ export default function Dashboard() {
             </form>
 
           </div>
+        </section>
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PÚBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={20} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus aperiam ipsum est, error sed exercitationem rem in suscipit sequi assumenda, vel nesciunt quaerat veritatis, quisquam voluptas repellat provident reiciendis impedit!</p>
+            <button className={styles.trashButton}>
+            <FaTrash size={20} color="#ea3140" />
+            </button>
+            </div>
+          </article>
         </section>
 
       </main>
